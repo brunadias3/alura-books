737f7315caf10ed2ecb0f55999acde44101295a3
Bruna <brunadias_30@hotmail.com>
2023-01-29 21:30:30 -0300

    estilizando

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -4,8 +4,15 @@ import logo from '../../imagens/logo.svg'
 export default function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='logo da alura books' />
-            <p><strong>Alura</strong> Books</p>
+            <img
+                src={logo}
+                alt='logo da alura books'
+                className='logo-img'
+            />
+
+            <p>
+                <strong>Alura</strong> Books
+            </p>
         </div>
     )
 }

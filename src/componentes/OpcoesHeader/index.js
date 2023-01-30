b09eb1cf8da167ada530e86aec4538f9ba77bd00
Bruna <brunadias_30@hotmail.com>
2023-01-30 20:35:56 -0300

    estilizando com styled components

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,30 @@
-import './estilo.css'
+import styled from "styled-components"
 
 export default function OpcoesHeader() {
 
     const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+    const StyledOpcoes = styled.ul`
+
+    display: flex;
+    
+    .opcao {
+        font-size: 16px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        height: 100%;
+        padding: 0 5px;
+        cursor: pointer;
+        min-width: 120px;
+    }
+`
 
     return (
-        <ul className='opcoes'>
+        <StyledOpcoes>
             {textoOpcoes.map((texto) => (
                 <li className='opcao'><p>{texto}</p></li>
             ))}
-        </ul>
+        </StyledOpcoes>
     )
 }
